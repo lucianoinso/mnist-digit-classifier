518e63bf84bbed53204da62c9ff1636f4c6f51cc
lucianoinso <lucianoinso@gmail.com>
2024-06-04 15:06:32 -0300

    now it returns a list with the top 2 most probable estimations, added requirements.txt file, removed unnecessary code

--- backend-tflite/main.py
+++ backend-tflite/main.py
@@ -1,5 +1,6 @@
+import os
 from flask import Flask, jsonify, request
-from flask_cors import cross_origin, CORS
+from flask_cors import CORS
 from mnist_classifier import predict
 import base64
 
@@ -9,21 +10,23 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'yoursecretkey')
 app.config['CORS_HEADERS'] = 'Content-Type'
 CORS(app)
 
+
 @app.route('/', methods=['GET', 'POST'])
-@cross_origin(origin='localhost', headers=['Content-Type', 'Authorization'])
 def return_prediction():
     if request.method == 'POST':
         b64_image = request.form.to_dict(flat=False)['imageBase64'][0][22:]
         img_filename = 'image.png'
         with open(img_filename, "wb") as imagefile:
             imagefile.write(base64.b64decode(b64_image))
-        prediction = predict(img_filename)
+        prediction = predict(img_filename).astype(str).tolist()
         response = {'message': prediction}
     elif request.method == 'GET':
-        response = {'message': 'server running!'}
+        response = {'message': 'Server running!'}
     else:
         response = {'message': "Method not allowed"}
+
     return jsonify(response)
+
 
 if __name__ == '__main__':
     app.run()
--- backend-tflite/mnist_classifier.py
+++ backend-tflite/mnist_classifier.py
@@ -1,10 +1,10 @@
 from os.path import exists
-from PIL import Image, ImageOps
+from PIL import Image
 import tflite_runtime.interpreter as tflite
 import numpy as np
 
 
-def predict():
+def predict(img_path):
     model_path = 'trained_model/mnist_predictor.tflite'
 
     if (not (exists(model_path))):
@@ -12,11 +12,9 @@ def predict():
               "exists inside 'trained_model' directory.")
         return
 
-    img_path = "./image.png"
     img = Image.open(img_path).convert('L').resize((28, 28),
                                                    Image.ANTIALIAS)
     img = np.array(img, dtype=np.float32)
-    res = Image.fromarray(img)
 
     # Load the TFLite model and allocate tensors.
     interpreter = tflite.Interpreter(model_path=model_path)
@@ -27,9 +25,7 @@ def predict():
     output_details = interpreter.get_output_details()
 
     input_shape = input_details[0]['shape']
-    input_data = img
-
-    input_tensor = img.reshape(input_details[0]['shape'])
+    input_tensor = img.reshape(input_shape)
 
     interpreter.set_tensor(input_details[0]['index'], input_tensor)
     interpreter.invoke()
@@ -41,9 +37,3 @@ def predict():
     top_2_results = np.argsort(logits)[0][::-1][:2]
 
     return top_2_results
-
-
-if __name__ == '__main__':
-    prediction = predict()
-    print("The top 2 most probable digits are: ")
-    print(prediction)
